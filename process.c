5b05d9c9e9efd536cd435ca21279d38884fb173d
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-09-26 14:30:48 +0300

    fix mistake

--- process.c
+++ process.c
@@ -2,43 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
 
     FILE *inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL) {
+    if (inputFile == NULL)
+    {
         perror("Error opening input file");
         return 2;
     }
 
     FILE *outputFile = fopen(argv[2], "w");
-    if (outputFile == NULL) {
+    if (outputFile == NULL)
+    {
         perror("Error opening output file");
         fclose(inputFile);
         return 3;
     }
 
-    
-    char buffer[21];  
-    size_t bytesRead = fread(buffer, 1, 20, inputFile);
-    buffer[bytesRead] = '\0';
+    int character;
+    int count = 0;
 
-   
-    fwrite(buffer, 1, bytesRead, outputFile);
+    while ((character = fgetc(inputFile)) != EOF)
+    {
+        if (character == 'a')
+        {
 
-    char *studentInfo = "Vasiuk Kateryna 335a";
-    int studentInfoLength = strlen(studentInfo);
+            fputs("Ao", outputFile);
+            count += 2;
+        }
+        else
+        {
 
-   
-    fwrite(studentInfo, 1, studentInfoLength, outputFile);
+            fputc(character, outputFile);
+            count++;
+        }
 
-   
-    char fileBuffer[1024];
-    while ((bytesRead = fread(fileBuffer, 1, sizeof(fileBuffer), inputFile)) > 0) {
-        fwrite(fileBuffer, 1, bytesRead, outputFile);
+        if (count == 20)
+        {
+            char *studentInfo = "Vasiuk Kateryna 335a";
+            fwrite(studentInfo, 1, strlen(studentInfo), outputFile);
+        }
     }
 
     fclose(inputFile);
